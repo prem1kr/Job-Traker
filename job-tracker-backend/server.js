14fff36a0d7610f11807d8817faefabc1159fed4
prem kumar <premkm608@gmail.com>
2025-04-11 10:05:21 +0530

    first commit

--- job-tracker-backend/server.js
+++ job-tracker-backend/server.js
@@ -6,7 +6,16 @@ const jobRoutes = require('./routes/jobRoutes');
 
 dotenv.config();
 const app = express();
+const corsOptions = {
+  origin: [
+    'https://job-traker-seven.vercel.app',
+    'http://localhost:3000'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
 
+app.use(cors(corsOptions));
 app.use(cors());
 app.use(express.json());
 app.use('/api/jobs', jobRoutes);
